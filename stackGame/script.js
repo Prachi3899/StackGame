0ea157d74ecbf3979210fac8a389f692a0d4f366
Prachi3899 <116147549+Prachi3899@users.noreply.github.com>
2024-12-04 23:57:19 +0530

    function

--- stackGame/script.js
+++ stackGame/script.js
@@ -8,7 +8,7 @@ let scrollCounter, camerY, current, mode, xSpeed
 let ySpeed = 5;
 let height = 50;
 
-let boxed = [];
+let boxes = [];
 boxes[0]={
     x:300,
     y:300,
@@ -20,10 +20,51 @@ let debris = {
     width:0
 }
 
-function newbox(){}
-function gameOver(){}
-function animate(){}
-function restart(){}
+function newBox(){
+    boxes[current]={
+        x:0,
+        y:(current + 10) * height,
+        width:boxes[current - 1].width
+    }
+}
+function gameOver(){
+    // show game over text 
+    gameOvertext.style.display = 'block'
+    mode = 'gameOver'
+}
+function animate(){
+    if(mode!= 'gameOver'){
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        scoreNumber.innerHtml = current - 1
+
+        for(let n=0; n<boxes.length;n++){
+            let box = boxes[n]
+            context.fillStyle = 'rgb(' + n * 16 +',' + n * 16 + ',' + n * 16 + ')';
+            context.fillRect(box.x,600 - box,y+camerY, box.width, height)
+        }
+
+        context.fillStyle = 'yellow'
+        context.fillRect(debris.x, 600 - debris.y + camerY, debris.width, height)    
+
+        if(mode == 'bounce'){
+            boxes[current].x = boxes[current].x + xSpeed;
+            if(xSpeed>0 && boxes[current].x + boxes[current].width > canvas.width)
+                xSpeed = -xSpeed
+        }
+    }
+    window.requestAnimationFrame(animate)
+}
+function restart(){
+    gameOvertext.style.display = 'none'
+    boxes.splice(1, boxes.length - 1)
+    mode = 'bounce'
+    camerY = 0
+    scrollCounter = 0
+    xSpeed = 2
+    current = 1
+    newBox()
+    debris.y=0
+}
 
 canvas.onpointerdown = function(e){
     if(mode == 'gameOver'){
